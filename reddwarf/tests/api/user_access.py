1d80b46e42094e13f7a30287a1450411b08e7cb4
David Fecker <david.fecker@rackspace.com>
2013-03-01 14:11:50 -0600

    Added tests for Modify User Call
    * Added line in guestagent fake to clear user grants after delete
    * Added negative tests for Modify User
    
    Change-Id: I16a6a9ebab281688c2a88c0c228e80337e05886e

--- reddwarf/tests/api/user_access.py
+++ reddwarf/tests/api/user_access.py
@@ -29,44 +29,71 @@ from reddwarf.tests.util import test_config
 from reddwarf.tests.api.users import TestUsers
 
 GROUP = "dbaas.api.useraccess"
+GROUP_POSITIVE = GROUP + ".positive"
+GROUP_NEGATIVE = GROUP + ".negative"
 
 
-@test(depends_on_classes=[TestUsers],
-      groups=[tests.DBAAS_API, GROUP, tests.INSTANCES],
-      runs_after=[TestUsers])
-class TestUserAccess(object):
+class UserAccessBase(object):
     """
-    Test the creation and deletion of user grants.
+    Base class for Positive and Negative TestUserAccess classes
     """
 
-    @before_class
-    def setUp(self):
+    def set_up(self):
         self.dbaas = util.create_dbaas_client(instance_info.user)
         self.users = ["test_access_user"]
         self.databases = [("test_access_db%02i" % i) for i in range(4)]
-        # None of the ghosts are real databases or users.
-        self.ghostdbs = ["test_user_access_ghost_db"]
-        self.ghostusers = ["test_ghostuser"]
-        self.revokedbs = self.databases[:1]
-        self.remainingdbs = self.databases[1:]
 
-    def _test_access(self, expecteddbs):
-        for user in self.users:
-            access = self.dbaas.users.list_access(instance_info.id, user)
-            assert_equal(200, self.dbaas.last_http_code)
-            access = [db.name for db in access]
-            assert_equal(set(access), set(expecteddbs))
+    def _user_list_from_names(self, usernames):
+        return [{"name": name,
+                 "password": "password",
+                 "databases": []} for name in usernames]
 
-    def _grant_access(self, databases):
-        for user in self.users:
+    def _grant_access_singular(self, user, databases, expected_response=202):
+        """ Grant a single user access to the databases listed.
+            Potentially, expect an exception in the process."""
+        try:
             self.dbaas.users.grant(instance_info.id, user, databases)
-            assert_equal(202, self.dbaas.last_http_code)
+        except exceptions.BadRequest as br:
+            assert_equal(400, expected_response)
+        except exceptions.NotFound as nf:
+            assert_equal(404, expected_response)
+        except exceptions.ClientException as ce:
+            assert_equal(500, expected_response)
+        finally:
+            assert_equal(expected_response, self.dbaas.last_http_code)
 
-    def _revoke_access(self, databases):
-        for user in self.users:
+    def _grant_access_plural(self, users, databases, expected_response=202):
+        """ Grant each user in the list access to all the databases listed.
+            Potentially, expect an exception in the process."""
+        for user in users:
+            self._grant_access_singular(user, databases, expected_response)
+
+    def _revoke_access_singular(self, user, database, expected_response=202):
+        """ Revoke from a user access to the given database .
+            Potentially, expect an exception in the process."""
+        try:
+            self.dbaas.users.revoke(instance_info.id, user, database)
+            assert_true(expected_response, self.dbaas.last_http_code)
+        except exceptions.NotFound as nf:
+            assert_equal(404, self.dbaas.last_http_code)
+
+    def _revoke_access_plural(self, users, databases, expected_response=202):
+        """ Revoke from each user access to each database.
+            Potentially, expect an exception in the process."""
+        for user in users:
             for database in databases:
-                self.dbaas.users.revoke(instance_info.id, user, database)
-                assert_true(self.dbaas.last_http_code in [202, 404])
+                self._revoke_access_singular(user,
+                                             database,
+                                             expected_response)
+
+    def _test_access(self, users, databases, expected_response=200):
+        """ Verify that each user in the list has access to each database in
+            the list."""
+        for user in users:
+            access = self.dbaas.users.list_access(instance_info.id, user)
+            assert_equal(expected_response, self.dbaas.last_http_code)
+            access = [db.name for db in access]
+            assert_equal(set(access), set(databases))
 
     def _reset_access(self):
         for user in self.users:
@@ -77,7 +104,25 @@ class TestUserAccess(object):
                 except exceptions.NotFound as nf:
                     # This is all right here, since we're resetting.
                     pass
-        self._test_access([])
+        self._test_access(self.users, [])
+
+
+@test(depends_on_classes=[TestUsers],
+      groups=[tests.DBAAS_API, GROUP, GROUP_POSITIVE, tests.INSTANCES],
+      runs_after=[TestUsers])
+class TestUserAccessPositive(UserAccessBase):
+    """
+    Test the creation and deletion of user grants.
+    """
+
+    @before_class
+    def setUp(self):
+        super(TestUserAccessPositive, self).set_up()
+        # None of the ghosts are real databases or users.
+        self.ghostdbs = ["test_user_access_ghost_db"]
+        self.ghostusers = ["test_ghostuser"]
+        self.revokedbs = self.databases[:1]
+        self.remainingdbs = self.databases[1:]
 
     def _ensure_nothing_else_created(self):
         # Make sure grants and revokes do not create users or databases.
@@ -92,12 +137,12 @@ class TestUserAccess(object):
 
     @test()
     def test_create_user_and_dbs(self):
-        users = [{"name": user, "password": "password", "databases": []}
-                 for user in self.users]
+        users = self._user_list_from_names(self.users)
         self.dbaas.users.create(instance_info.id, users)
         assert_equal(202, self.dbaas.last_http_code)
 
-        databases = [{"name": db} for db in self.databases]
+        databases = [{"name": db}
+                     for db in self.databases]
         self.dbaas.databases.create(instance_info.id, databases)
         assert_equal(202, self.dbaas.last_http_code)
 
@@ -105,57 +150,55 @@ class TestUserAccess(object):
     def test_no_access(self):
         # No users have any access to any database.
         self._reset_access()
-        self._test_access([])
+        self._test_access(self.users, [])
 
     @test(depends_on=[test_no_access])
     def test_grant_full_access(self):
         # The users are granted access to all test databases.
         self._reset_access()
-        self._grant_access(self.databases)
-        self._test_access(self.databases)
+        self._grant_access_plural(self.users, self.databases)
+        self._test_access(self.users, self.databases)
 
     @test(depends_on=[test_grant_full_access])
     def test_grant_idempotence(self):
         # Grant operations can be repeated with no ill effects.
         self._reset_access()
-        self._grant_access(self.databases)
-        self._grant_access(self.databases)
-        self._test_access(self.databases)
+        for repeat in range(3):
+            self._grant_access_plural(self.users, self.databases)
+        self._test_access(self.users, self.databases)
 
     @test(depends_on=[test_grant_full_access])
     def test_revoke_one_database(self):
         # Revoking permission removes that database from a user's list.
         self._reset_access()
-        self._grant_access(self.databases)
-        self._test_access(self.databases)
-        self._revoke_access(self.revokedbs)
-        self._test_access(self.remainingdbs)
+        self._grant_access_plural(self.users, self.databases)
+        self._test_access(self.users, self.databases)
+        self._revoke_access_plural(self.users, self.revokedbs)
+        self._test_access(self.users, self.remainingdbs)
 
     @test(depends_on=[test_grant_full_access])
     def test_revoke_non_idempotence(self):
         # Revoking access cannot be repeated.
         self._reset_access()
-        self._grant_access(self.databases)
-        self._revoke_access(self.revokedbs)
-        assert_raises(exceptions.NotFound,
-                      self._revoke_access,
-                      self.revokedbs)
-        self._test_access(self.remainingdbs)
+        self._grant_access_plural(self.users, self.databases)
+        self._revoke_access_plural(self.users, self.revokedbs)
+        self._revoke_access_plural(self.users, self.revokedbs, 404)
+        self._test_access(self.users, self.remainingdbs)
 
     @test(depends_on=[test_grant_full_access])
     def test_revoke_all_access(self):
         # Revoking access to all databases will leave their access empty.
         self._reset_access()
-        self._grant_access(self.databases)
-        self._revoke_access(self.databases)
-        self._test_access([])
+        self._grant_access_plural(self.users, self.databases)
+        self._revoke_access_plural(self.users, self.revokedbs)
+        self._test_access(self.users, self.remainingdbs)
 
     @test(depends_on=[test_grant_full_access])
     def test_grant_ghostdbs(self):
         # Grants to imaginary databases are acceptable, and are honored.
         self._reset_access()
         self._ensure_nothing_else_created()
-        self._grant_access(self.ghostdbs)
+        self._grant_access_plural(self.users, self.ghostdbs)
         self._ensure_nothing_else_created()
 
     @test(depends_on=[test_grant_full_access])
@@ -163,8 +206,8 @@ class TestUserAccess(object):
         # Revokes to imaginary databases are acceptable, and are honored.
         self._reset_access()
         self._ensure_nothing_else_created()
-        self._grant_access(self.ghostdbs)
-        self._revoke_access(self.ghostdbs)
+        self._grant_access_plural(self.users, self.ghostdbs)
+        self._revoke_access_plural(self.users, self.ghostdbs)
         self._ensure_nothing_else_created()
 
     @test(depends_on=[test_grant_full_access])
@@ -172,23 +215,133 @@ class TestUserAccess(object):
         # You cannot grant permissions to imaginary users, as imaginary users
         # don't have passwords we can pull from mysql.users
         self._reset_access()
-        for user in self.ghostusers:
-            assert_raises(exceptions.NotFound,
-                          self.dbaas.users.grant,
-                          instance_info.id, user, self.databases)
-            assert_equal(404, self.dbaas.last_http_code)
+        self._grant_access_plural(self.ghostusers, self.databases, 404)
 
     @test(depends_on=[test_grant_full_access])
     def test_revoke_ghostusers(self):
         # You cannot revoke permissions from imaginary users, as imaginary
         # users don't have passwords we can pull from mysql.users
         self._reset_access()
-        for user in self.ghostusers:
-            for database in self.databases:
-                assert_raises(exceptions.NotFound,
-                              self.dbaas.users.revoke,
-                              instance_info.id, user, database)
-                assert_equal(404, self.dbaas.last_http_code)
+        self._revoke_access_plural(self.ghostusers, self.databases, 404)
+
+    @after_class(always_run=True)
+    def tearDown(self):
+        self._reset_access()
+        for database in self.databases:
+            self.dbaas.databases.delete(instance_info.id, database)
+            assert_equal(202, self.dbaas.last_http_code)
+
+
+@test(depends_on_classes=[TestUserAccessPositive],
+      groups=[tests.DBAAS_API, GROUP, GROUP_NEGATIVE, tests.INSTANCES],
+      runs_after=[TestUserAccessPositive])
+class TestUserAccessNegative(UserAccessBase):
+    """
+    Negative tests for the creation and deletion of user grants.
+    """
+
+    @before_class
+    def setUp(self):
+        super(TestUserAccessNegative, self).set_up()
+        self.users = ["qe_user?neg3F", "qe_user#neg23"]
+        self.databases = [("qe_user_neg_db%02i" % i) for i in range(2)]
+        self.ghostdbs = []
+
+    def _add_users(self, users, expected_response=202):
+        user_list = self._user_list_from_names(users)
+        try:
+            self.dbaas.users.create(instance_info.id, user_list)
+            assert_equal(self.dbaas.last_http_code, 202)
+        except exceptions.BadRequest as br:
+            assert_equal(self.dbaas.last_http_code, 400)
+        assert_equal(expected_response, self.dbaas.last_http_code)
+
+    @test()
+    def test_create_duplicate_user_and_dbs(self):
+        '''
+        create the same user to the first DB - allowed, not part of change
+        '''
+        users = self._user_list_from_names(self.users)
+        self.dbaas.users.create(instance_info.id, users)
+        assert_equal(202, self.dbaas.last_http_code)
+        databases = [{"name": db} for db in self.databases]
+        self.dbaas.databases.create(instance_info.id, databases)
+        assert_equal(202, self.dbaas.last_http_code)
+
+    @test(depends_on=[test_create_duplicate_user_and_dbs])
+    def test_neg_duplicate_useraccess(self):
+        '''
+        Grant duplicate users access to all database.
+        '''
+        username = "qe_user.neg2E"
+        self._add_users([username])
+        self._add_users([username], 400)
+        for repeat in range(3):
+            self._grant_access_plural(self.users, self.databases)
+        self._test_access(self.users, self.databases)
+
+    @test()
+    def test_re_create_user(self):
+        user_list = ["re_create_user"]
+        # create, grant, then check a new user
+        self._add_users(user_list)
+        self._test_access(user_list, [])
+        self._grant_access_singular(user_list[0], self.databases)
+        self._test_access(user_list, self.databases)
+        # drop the user temporarily
+        self.dbaas.users.delete(instance_info.id, user_list[0])
+        # check his access - user should not be found
+        assert_raises(exceptions.NotFound,
+                      self.dbaas.users.list_access,
+                      instance_info.id,
+                      user_list[0])
+        # re-create the user
+        self._add_users(user_list)
+        # check his access - should not exist
+        self._test_access(user_list, [])
+        # grant user access to all database.
+        self._grant_access_singular(user_list[0], self.databases)
+        # check his access - user should exist
+        self._test_access(user_list, self.databases)
+        # revoke users access
+        self._revoke_access_plural(user_list, self.databases)
+
+    def _negative_user_test(self, username, databases,
+                            create_response=202, grant_response=202,
+                            access_response=200, revoke_response=202):
+        # Try and fail to create the user.
+        self._add_users([username], create_response)
+        self._grant_access_singular(username, databases, grant_response)
+        access = None
+        try:
+            access = self.dbaas.users.list_access(instance_info.id, username)
+            assert_equal(200, self.dbaas.last_http_code)
+        except exceptions.NotFound as nf:
+            assert_equal(404, self.dbaas.last_http_code)
+        finally:
+            assert_equal(access_response, self.dbaas.last_http_code)
+        if access is not None:
+            access = [db.name for db in access]
+            assert_equal(set(access), set(self.databases))
+
+        self._revoke_access_plural([username], databases, revoke_response)
+
+    @test
+    def test_user_withperiod(self):
+        self._negative_user_test("test.user", self.databases)
+
+    @test
+    def test_user_empty(self):
+        self._negative_user_test("", self.databases, 400, 500, 404, 404)
+
+    @test
+    def test_user_nametoolong(self):
+        self._negative_user_test("exceed_limit_user", self.databases,
+                                 400, 404, 404, 404)
+
+    @test
+    def test_user_allspaces(self):
+        self._negative_user_test("     ", self.databases, 400, 404, 404, 404)
 
     @after_class(always_run=True)
     def tearDown(self):
