f14c18852f8a1f1af8b19a4b5327787d8f439158
Paul Marshall <paul.marshall@rackspace.com>
2013-08-16 14:02:49 -0500

    Ensure safe format strings for TroveError
    Fixes: bug #1184044
    Change-Id: I80f2ac4b87020dd0994c8aee85025dad75d5a3ef

---
+++ trove/tests/unittests/common/test_exception.py
@@ -0,0 +1,28 @@
+#    Copyright 2013 OpenStack LLC
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+from testtools import TestCase
+from trove.common.exception import TroveError
+
+
+class TroveErrorTest(TestCase):
+
+    def test_valid_error_message_format(self):
+        error = TroveError("%02d" % 1)
+        self.assertEqual(error.message, "01")
+
+    def test_invalid_error_message_format(self):
+        error = TroveError("test%999999sdb")
+        self.assertEqual(error.message, "test999999sdb")
