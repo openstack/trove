66b313c635af4d225d9532976ce31857d200b790
Chang Bo Guo <guochbo@cn.ibm.com>
2013-10-08 03:45:21 -0700

    Replace deprecated method aliases in tests
    Some of the TestCase methods have been deprecated since python 2.7.
    We need use correct names.
    
    http://docs.python.org/2/library/unittest.html#deprecated-aliases
    
    * replace 'assertEquals' with 'assertEqual'
    * replace 'failUnlessAlmostEqual' with 'assertAlmostEqual'
    
    Change-Id: I90da87723e3e36767946d276301b75af801b952d

--- trove/tests/unittests/quota/test_quota.py
+++ trove/tests/unittests/quota/test_quota.py
@@ -108,7 +108,7 @@ class QuotaControllerTest(testtools.TestCase):
         result = self.controller.update(self.req, body, FAKE_TENANT1,
                                         FAKE_TENANT2)
         verify(quota, never).save()
-        self.assertEquals(200, result.status)
+        self.assertEqual(200, result.status)
 
     def test_update_resource_instance(self):
         instance_quota = mock(Quota)
@@ -120,8 +120,8 @@ class QuotaControllerTest(testtools.TestCase):
                                         FAKE_TENANT2)
         verify(instance_quota, times=1).save()
         self.assertTrue('instances' in result._data['quotas'])
-        self.assertEquals(200, result.status)
-        self.assertEquals(2, result._data['quotas']['instances'])
+        self.assertEqual(200, result.status)
+        self.assertEqual(2, result._data['quotas']['instances'])
 
     @testtools.skipIf(not CONF.trove_volume_support,
                       'Volume support is not enabled')
@@ -140,8 +140,8 @@ class QuotaControllerTest(testtools.TestCase):
         verify(instance_quota, never).save()
         self.assertFalse('instances' in result._data['quotas'])
         verify(volume_quota, times=1).save()
-        self.assertEquals(200, result.status)
-        self.assertEquals(10, result._data['quotas']['volumes'])
+        self.assertEqual(200, result.status)
+        self.assertEqual(10, result._data['quotas']['volumes'])
 
 
 class DbQuotaDriverTest(testtools.TestCase):
@@ -190,9 +190,9 @@ class DbQuotaDriverTest(testtools.TestCase):
         quota = self.driver.get_quota_by_tenant(FAKE_TENANT1,
                                                 Resource.VOLUMES)
 
-        self.assertEquals(FAKE_TENANT1, quota.tenant_id)
-        self.assertEquals(Resource.INSTANCES, quota.resource)
-        self.assertEquals(12, quota.hard_limit)
+        self.assertEqual(FAKE_TENANT1, quota.tenant_id)
+        self.assertEqual(Resource.INSTANCES, quota.resource)
+        self.assertEqual(12, quota.hard_limit)
 
     def test_get_quota_by_tenant_default(self):
 
@@ -201,9 +201,9 @@ class DbQuotaDriverTest(testtools.TestCase):
         quota = self.driver.get_quota_by_tenant(FAKE_TENANT1,
                                                 Resource.VOLUMES)
 
-        self.assertEquals(FAKE_TENANT1, quota.tenant_id)
-        self.assertEquals(Resource.VOLUMES, quota.resource)
-        self.assertEquals(CONF.max_volumes_per_user, quota.hard_limit)
+        self.assertEqual(FAKE_TENANT1, quota.tenant_id)
+        self.assertEqual(Resource.VOLUMES, quota.resource)
+        self.assertEqual(CONF.max_volumes_per_user, quota.hard_limit)
 
     def test_get_all_quotas_by_tenant(self):
 
@@ -219,13 +219,13 @@ class DbQuotaDriverTest(testtools.TestCase):
         quotas = self.driver.get_all_quotas_by_tenant(FAKE_TENANT1,
                                                       resources.keys())
 
-        self.assertEquals(FAKE_TENANT1, quotas[Resource.INSTANCES].tenant_id)
-        self.assertEquals(Resource.INSTANCES,
-                          quotas[Resource.INSTANCES].resource)
-        self.assertEquals(22, quotas[Resource.INSTANCES].hard_limit)
-        self.assertEquals(FAKE_TENANT1, quotas[Resource.VOLUMES].tenant_id)
-        self.assertEquals(Resource.VOLUMES, quotas[Resource.VOLUMES].resource)
-        self.assertEquals(15, quotas[Resource.VOLUMES].hard_limit)
+        self.assertEqual(FAKE_TENANT1, quotas[Resource.INSTANCES].tenant_id)
+        self.assertEqual(Resource.INSTANCES,
+                         quotas[Resource.INSTANCES].resource)
+        self.assertEqual(22, quotas[Resource.INSTANCES].hard_limit)
+        self.assertEqual(FAKE_TENANT1, quotas[Resource.VOLUMES].tenant_id)
+        self.assertEqual(Resource.VOLUMES, quotas[Resource.VOLUMES].resource)
+        self.assertEqual(15, quotas[Resource.VOLUMES].hard_limit)
 
     def test_get_all_quotas_by_tenant_with_all_default(self):
 
@@ -234,15 +234,15 @@ class DbQuotaDriverTest(testtools.TestCase):
         quotas = self.driver.get_all_quotas_by_tenant(FAKE_TENANT1,
                                                       resources.keys())
 
-        self.assertEquals(FAKE_TENANT1, quotas[Resource.INSTANCES].tenant_id)
-        self.assertEquals(Resource.INSTANCES,
-                          quotas[Resource.INSTANCES].resource)
-        self.assertEquals(CONF.max_instances_per_user,
-                          quotas[Resource.INSTANCES].hard_limit)
-        self.assertEquals(FAKE_TENANT1, quotas[Resource.VOLUMES].tenant_id)
-        self.assertEquals(Resource.VOLUMES, quotas[Resource.VOLUMES].resource)
-        self.assertEquals(CONF.max_volumes_per_user,
-                          quotas[Resource.VOLUMES].hard_limit)
+        self.assertEqual(FAKE_TENANT1, quotas[Resource.INSTANCES].tenant_id)
+        self.assertEqual(Resource.INSTANCES,
+                         quotas[Resource.INSTANCES].resource)
+        self.assertEqual(CONF.max_instances_per_user,
+                         quotas[Resource.INSTANCES].hard_limit)
+        self.assertEqual(FAKE_TENANT1, quotas[Resource.VOLUMES].tenant_id)
+        self.assertEqual(Resource.VOLUMES, quotas[Resource.VOLUMES].resource)
+        self.assertEqual(CONF.max_volumes_per_user,
+                         quotas[Resource.VOLUMES].hard_limit)
 
     def test_get_all_quotas_by_tenant_with_one_default(self):
 
@@ -255,14 +255,14 @@ class DbQuotaDriverTest(testtools.TestCase):
         quotas = self.driver.get_all_quotas_by_tenant(FAKE_TENANT1,
                                                       resources.keys())
 
-        self.assertEquals(FAKE_TENANT1, quotas[Resource.INSTANCES].tenant_id)
-        self.assertEquals(Resource.INSTANCES,
-                          quotas[Resource.INSTANCES].resource)
-        self.assertEquals(22, quotas[Resource.INSTANCES].hard_limit)
-        self.assertEquals(FAKE_TENANT1, quotas[Resource.VOLUMES].tenant_id)
-        self.assertEquals(Resource.VOLUMES, quotas[Resource.VOLUMES].resource)
-        self.assertEquals(CONF.max_volumes_per_user,
-                          quotas[Resource.VOLUMES].hard_limit)
+        self.assertEqual(FAKE_TENANT1, quotas[Resource.INSTANCES].tenant_id)
+        self.assertEqual(Resource.INSTANCES,
+                         quotas[Resource.INSTANCES].resource)
+        self.assertEqual(22, quotas[Resource.INSTANCES].hard_limit)
+        self.assertEqual(FAKE_TENANT1, quotas[Resource.VOLUMES].tenant_id)
+        self.assertEqual(Resource.VOLUMES, quotas[Resource.VOLUMES].resource)
+        self.assertEqual(CONF.max_volumes_per_user,
+                         quotas[Resource.VOLUMES].hard_limit)
 
     def test_get_quota_usage_by_tenant(self):
 
@@ -276,10 +276,10 @@ class DbQuotaDriverTest(testtools.TestCase):
         usage = self.driver.get_quota_usage_by_tenant(FAKE_TENANT1,
                                                       Resource.VOLUMES)
 
-        self.assertEquals(FAKE_TENANT1, usage.tenant_id)
-        self.assertEquals(Resource.VOLUMES, usage.resource)
-        self.assertEquals(3, usage.in_use)
-        self.assertEquals(1, usage.reserved)
+        self.assertEqual(FAKE_TENANT1, usage.tenant_id)
+        self.assertEqual(Resource.VOLUMES, usage.resource)
+        self.assertEqual(3, usage.in_use)
+        self.assertEqual(1, usage.reserved)
 
     def test_get_quota_usage_by_tenant_default(self):
 
@@ -294,10 +294,10 @@ class DbQuotaDriverTest(testtools.TestCase):
         usage = self.driver.get_quota_usage_by_tenant(FAKE_TENANT1,
                                                       Resource.VOLUMES)
 
-        self.assertEquals(FAKE_TENANT1, usage.tenant_id)
-        self.assertEquals(Resource.VOLUMES, usage.resource)
-        self.assertEquals(0, usage.in_use)
-        self.assertEquals(0, usage.reserved)
+        self.assertEqual(FAKE_TENANT1, usage.tenant_id)
+        self.assertEqual(Resource.VOLUMES, usage.resource)
+        self.assertEqual(0, usage.in_use)
+        self.assertEqual(0, usage.reserved)
 
     def test_get_all_quota_usages_by_tenant(self):
 
@@ -315,15 +315,15 @@ class DbQuotaDriverTest(testtools.TestCase):
         usages = self.driver.get_all_quota_usages_by_tenant(FAKE_TENANT1,
                                                             resources.keys())
 
-        self.assertEquals(FAKE_TENANT1, usages[Resource.INSTANCES].tenant_id)
-        self.assertEquals(Resource.INSTANCES,
-                          usages[Resource.INSTANCES].resource)
-        self.assertEquals(2, usages[Resource.INSTANCES].in_use)
-        self.assertEquals(1, usages[Resource.INSTANCES].reserved)
-        self.assertEquals(FAKE_TENANT1, usages[Resource.VOLUMES].tenant_id)
-        self.assertEquals(Resource.VOLUMES, usages[Resource.VOLUMES].resource)
-        self.assertEquals(1, usages[Resource.VOLUMES].in_use)
-        self.assertEquals(1, usages[Resource.VOLUMES].reserved)
+        self.assertEqual(FAKE_TENANT1, usages[Resource.INSTANCES].tenant_id)
+        self.assertEqual(Resource.INSTANCES,
+                         usages[Resource.INSTANCES].resource)
+        self.assertEqual(2, usages[Resource.INSTANCES].in_use)
+        self.assertEqual(1, usages[Resource.INSTANCES].reserved)
+        self.assertEqual(FAKE_TENANT1, usages[Resource.VOLUMES].tenant_id)
+        self.assertEqual(Resource.VOLUMES, usages[Resource.VOLUMES].resource)
+        self.assertEqual(1, usages[Resource.VOLUMES].in_use)
+        self.assertEqual(1, usages[Resource.VOLUMES].reserved)
 
     def test_get_all_quota_usages_by_tenant_with_all_default(self):
 
@@ -342,15 +342,15 @@ class DbQuotaDriverTest(testtools.TestCase):
         usages = self.driver.get_all_quota_usages_by_tenant(FAKE_TENANT1,
                                                             resources.keys())
 
-        self.assertEquals(FAKE_TENANT1, usages[Resource.INSTANCES].tenant_id)
-        self.assertEquals(Resource.INSTANCES,
-                          usages[Resource.INSTANCES].resource)
-        self.assertEquals(0, usages[Resource.INSTANCES].in_use)
-        self.assertEquals(0, usages[Resource.INSTANCES].reserved)
-        self.assertEquals(FAKE_TENANT1, usages[Resource.VOLUMES].tenant_id)
-        self.assertEquals(Resource.VOLUMES, usages[Resource.VOLUMES].resource)
-        self.assertEquals(0, usages[Resource.VOLUMES].in_use)
-        self.assertEquals(0, usages[Resource.VOLUMES].reserved)
+        self.assertEqual(FAKE_TENANT1, usages[Resource.INSTANCES].tenant_id)
+        self.assertEqual(Resource.INSTANCES,
+                         usages[Resource.INSTANCES].resource)
+        self.assertEqual(0, usages[Resource.INSTANCES].in_use)
+        self.assertEqual(0, usages[Resource.INSTANCES].reserved)
+        self.assertEqual(FAKE_TENANT1, usages[Resource.VOLUMES].tenant_id)
+        self.assertEqual(Resource.VOLUMES, usages[Resource.VOLUMES].resource)
+        self.assertEqual(0, usages[Resource.VOLUMES].in_use)
+        self.assertEqual(0, usages[Resource.VOLUMES].reserved)
 
     def test_get_all_quota_usages_by_tenant_with_one_default(self):
 
@@ -369,15 +369,15 @@ class DbQuotaDriverTest(testtools.TestCase):
         usages = self.driver.get_all_quota_usages_by_tenant(FAKE_TENANT1,
                                                             resources.keys())
 
-        self.assertEquals(FAKE_TENANT1, usages[Resource.INSTANCES].tenant_id)
-        self.assertEquals(Resource.INSTANCES,
-                          usages[Resource.INSTANCES].resource)
-        self.assertEquals(0, usages[Resource.INSTANCES].in_use)
-        self.assertEquals(0, usages[Resource.INSTANCES].reserved)
-        self.assertEquals(FAKE_TENANT1, usages[Resource.VOLUMES].tenant_id)
-        self.assertEquals(Resource.VOLUMES, usages[Resource.VOLUMES].resource)
-        self.assertEquals(0, usages[Resource.VOLUMES].in_use)
-        self.assertEquals(0, usages[Resource.VOLUMES].reserved)
+        self.assertEqual(FAKE_TENANT1, usages[Resource.INSTANCES].tenant_id)
+        self.assertEqual(Resource.INSTANCES,
+                         usages[Resource.INSTANCES].resource)
+        self.assertEqual(0, usages[Resource.INSTANCES].in_use)
+        self.assertEqual(0, usages[Resource.INSTANCES].reserved)
+        self.assertEqual(FAKE_TENANT1, usages[Resource.VOLUMES].tenant_id)
+        self.assertEqual(Resource.VOLUMES, usages[Resource.VOLUMES].resource)
+        self.assertEqual(0, usages[Resource.VOLUMES].in_use)
+        self.assertEqual(0, usages[Resource.VOLUMES].reserved)
 
     def test_reserve(self):
 
@@ -400,13 +400,13 @@ class DbQuotaDriverTest(testtools.TestCase):
         delta = {'instances': 2, 'volumes': 3}
         self.driver.reserve(FAKE_TENANT1, resources, delta)
         _, kw = Reservation.create.call_args_list[0]
-        self.assertEquals(1, kw['usage_id'])
-        self.assertEquals(2, kw['delta'])
-        self.assertEquals(Reservation.Statuses.RESERVED, kw['status'])
+        self.assertEqual(1, kw['usage_id'])
+        self.assertEqual(2, kw['delta'])
+        self.assertEqual(Reservation.Statuses.RESERVED, kw['status'])
         _, kw = Reservation.create.call_args_list[1]
-        self.assertEquals(2, kw['usage_id'])
-        self.assertEquals(3, kw['delta'])
-        self.assertEquals(Reservation.Statuses.RESERVED, kw['status'])
+        self.assertEqual(2, kw['usage_id'])
+        self.assertEqual(3, kw['delta'])
+        self.assertEqual(Reservation.Statuses.RESERVED, kw['status'])
 
     def test_reserve_resource_unknown(self):
 
@@ -508,13 +508,13 @@ class DbQuotaDriverTest(testtools.TestCase):
         delta = {'instances': -1, 'volumes': -3}
         self.driver.reserve(FAKE_TENANT1, resources, delta)
         _, kw = Reservation.create.call_args_list[0]
-        self.assertEquals(1, kw['usage_id'])
-        self.assertEquals(-1, kw['delta'])
-        self.assertEquals(Reservation.Statuses.RESERVED, kw['status'])
+        self.assertEqual(1, kw['usage_id'])
+        self.assertEqual(-1, kw['delta'])
+        self.assertEqual(Reservation.Statuses.RESERVED, kw['status'])
         _, kw = Reservation.create.call_args_list[1]
-        self.assertEquals(2, kw['usage_id'])
-        self.assertEquals(-3, kw['delta'])
-        self.assertEquals(Reservation.Statuses.RESERVED, kw['status'])
+        self.assertEqual(2, kw['usage_id'])
+        self.assertEqual(-3, kw['delta'])
+        self.assertEqual(Reservation.Statuses.RESERVED, kw['status'])
 
     def test_commit(self):
 
