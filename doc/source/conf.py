d1d517a544bd949a7e47f6086480981b425070a4
Sergey Lukjanov <slukjanov@mirantis.com>
2014-03-26 15:33:50 +0400

    Start using oslosphinx theme for docs
    Change-Id: I0d08e7abbbf51d526d379e8fcea7151ba16d1746

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,7 +15,8 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.intersphinx',
               'sphinx.ext.todo',
-              'sphinx.ext.viewcode']
+              'sphinx.ext.viewcode',
+              'oslosphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -85,9 +86,9 @@ modindex_common_prefix = ['trove.']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme_path = ["."]
-html_theme = '_theme'
-html_static_path = ['_static']
+# html_theme_path = ["."]
+# html_theme = '_theme'
+# html_static_path = ['_static']
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -116,7 +117,7 @@ html_static_path = ['_static']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
