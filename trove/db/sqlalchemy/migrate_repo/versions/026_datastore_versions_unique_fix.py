c04226eb4d0ea64c7cf190b858c09464b8e9e98f
Doug Shelley <doug@parelastic.com>
2014-05-08 12:35:17 -0400

    Datastore_versions.name shouldn't be unique
    The name column in the datastore_versions table is not supposed
    to be defined with a unique index. The migrate version 18 was
    intended to fix this but the method used to correct the column
    definition doesn't work. Added a migrate version 26 to use
    UniqueConstraint.drop() instead.
    
    After some discussion, it was decided that downgrade would be
    a no-op because it is difficult to reliably create a unique
    index on a column with data in it, and further, the column wasn't
    supposed to be unique in the first place.
    
    Change-Id: Ida552982de6702db6ed4d40c0167e7d542c0ded4
    Closes-Bug: #1310834

---
+++ trove/db/sqlalchemy/migrate_repo/versions/026_datastore_versions_unique_fix.py
@@ -0,0 +1,44 @@
+# Copyright 2014 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from migrate.changeset import UniqueConstraint
+from sqlalchemy.exc import OperationalError
+from sqlalchemy.schema import MetaData
+
+from trove.db.sqlalchemy.migrate_repo.schema import Table
+from trove.openstack.common import log as logging
+
+logger = logging.getLogger('trove.db.sqlalchemy.migrate_repo.schema')
+
+
+def upgrade(migrate_engine):
+    meta = MetaData()
+    meta.bind = migrate_engine
+    datastore_versions = Table('datastore_versions', meta, autoload=True)
+
+    # drop the unique index on the name column - unless we are
+    # using sqlite - it doesn't support dropping unique constraints
+    if migrate_engine.name != "sqlite":
+        uc = UniqueConstraint('name', table=datastore_versions, name='name')
+        try:
+            uc.drop()
+        except OperationalError as e:
+            logger.info(e)
+
+
+def downgrade(migrate_engine):
+    # we aren't going to recreate the index in this case for 2 reasons:
+    # 1. this column being unique was a bug in the first place
+    # 2. adding a unique index to a column that has duplicates will fail
+    pass
