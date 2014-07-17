5b0f26aac261eb5cd5bc81f687f2175ff84097c0
Amrith Kumar <amrith@tesora.com>
2014-07-17 15:17:37 -0400

    Logging audit for guestagent/redis
    Adjust logging to conform to logging standards.
    Cleaned up a few messages that were unclear and changed some
    LOG.info messages to LOG.debug
    
    Change-Id: I3570dddf0ba7c576b4cf9cb6affa55335fd23ac7
    Partial-Bug: #1324206

--- trove/guestagent/datastore/redis/manager.py
+++ trove/guestagent/datastore/redis/manager.py
@@ -43,6 +43,7 @@ class Manager(periodic_task.PeriodicTasks):
         Updates the redis trove instance. It is decorated with
         perodic task so it is automatically called every 3 ticks.
         """
+        LOG.debug("Update status called.")
         RedisAppStatus.get().update()
 
     def change_passwords(self, context, users):
@@ -50,6 +51,7 @@ class Manager(periodic_task.PeriodicTasks):
         Changes the redis instance password,
         it is currently not not implemented.
         """
+        LOG.debug("Change passwords called.")
         raise exception.DatastoreOperationNotSupported(
             operation='change_passwords', datastore=MANAGER)
 
@@ -58,6 +60,7 @@ class Manager(periodic_task.PeriodicTasks):
         Resets to the default configuration,
         currently this does nothing.
         """
+        LOG.debug("Reset configuration called.")
         app = RedisApp(RedisAppStatus.get())
         app.reset_configuration(configuration)
 
@@ -66,6 +69,7 @@ class Manager(periodic_task.PeriodicTasks):
         Perform a restore on this instance,
         currently it is not implemented.
         """
+        LOG.debug("Perform restore called.")
         raise exception.DatastoreOperationNotSupported(
             operation='_perform_restore', datastore=MANAGER)
 
@@ -90,12 +94,12 @@ class Manager(periodic_task.PeriodicTasks):
                 operating_system.update_owner('redis', 'redis', mount_point)
                 LOG.debug('Mounted the volume.')
             app.install_if_needed(packages)
-            LOG.info(_('Securing redis now.'))
+            LOG.info(_('Writing redis configuration.'))
             app.write_config(config_contents)
             app.restart()
-            LOG.info(_('"prepare" redis call has finished.'))
-        except Exception as e:
-            LOG.error(e)
+            LOG.info(_('Redis instance has been setup and configured.'))
+        except Exception:
+            LOG.exception(_("Error setting up Redis instance."))
             app.status.set_status(rd_instance.ServiceStatuses.FAILED)
             raise RuntimeError("prepare call has failed.")
 
@@ -105,6 +109,7 @@ class Manager(periodic_task.PeriodicTasks):
         This method is called when the guest agent
         gets a restart message from the taskmanager.
         """
+        LOG.debug("Restart called.")
         app = RedisApp(RedisAppStatus.get())
         app.restart()
 
@@ -112,6 +117,7 @@ class Manager(periodic_task.PeriodicTasks):
         """
         Start this redis instance with new conf changes.
         """
+        LOG.debug("Start DB with conf changes called.")
         app = RedisApp(RedisAppStatus.get())
         app.start_db_with_conf_changes(config_contents)
 
@@ -121,11 +127,13 @@ class Manager(periodic_task.PeriodicTasks):
         This method is called when the guest agent
         gets a stop message from the taskmanager.
         """
+        LOG.debug("Stop DB called.")
         app = RedisApp(RedisAppStatus.get())
         app.stop_db(do_not_start_on_reboot=do_not_start_on_reboot)
 
     def get_filesystem_stats(self, context, fs_path):
         """Gets the filesystem stats for the path given."""
+        LOG.debug("Get Filesystem Stats.")
         mount_point = CONF.get(
             'mysql' if not MANAGER else MANAGER).mount_point
         return dbaas.get_filesystem_volume_stats(mount_point)
@@ -135,99 +143,121 @@ class Manager(periodic_task.PeriodicTasks):
         This will eventually create a backup. Right now
         it does nothing.
         """
+        LOG.debug("Create Backup called.")
         raise exception.DatastoreOperationNotSupported(
             operation='create_backup', datastore=MANAGER)
 
     def mount_volume(self, context, device_path=None, mount_point=None):
         device = volume.VolumeDevice(device_path)
         device.mount(mount_point, write_to_fstab=False)
-        LOG.debug("Mounted the volume.")
+        LOG.debug("Mounted the device %s at the mount point %s." %
+                  (device_path, mount_point))
 
     def unmount_volume(self, context, device_path=None, mount_point=None):
         device = volume.VolumeDevice(device_path)
         device.unmount(mount_point)
-        LOG.debug("Unmounted the volume.")
+        LOG.debug("Unmounted the device %s from the mount point %s." %
+                  (device_path, mount_point))
 
     def resize_fs(self, context, device_path=None, mount_point=None):
         device = volume.VolumeDevice(device_path)
         device.resize_fs(mount_point)
-        LOG.debug("Resized the filesystem")
+        LOG.debug("Resized the filesystem at %s." % mount_point)
 
     def update_overrides(self, context, overrides, remove=False):
+        LOG.debug("Updating overrides.")
         raise exception.DatastoreOperationNotSupported(
             operation='update_overrides', datastore=MANAGER)
 
     def apply_overrides(self, context, overrides):
+        LOG.debug("Applying overrides.")
         raise exception.DatastoreOperationNotSupported(
             operation='apply_overrides', datastore=MANAGER)
 
     def update_attributes(self, context, username, hostname, user_attrs):
+        LOG.debug("Updating attributes.")
         raise exception.DatastoreOperationNotSupported(
             operation='update_attributes', datastore=MANAGER)
 
     def create_database(self, context, databases):
+        LOG.debug("Creating database.")
         raise exception.DatastoreOperationNotSupported(
             operation='create_database', datastore=MANAGER)
 
     def create_user(self, context, users):
+        LOG.debug("Creating user.")
         raise exception.DatastoreOperationNotSupported(
             operation='create_user', datastore=MANAGER)
 
     def delete_database(self, context, database):
+        LOG.debug("Deleting database.")
         raise exception.DatastoreOperationNotSupported(
             operation='delete_database', datastore=MANAGER)
 
     def delete_user(self, context, user):
+        LOG.debug("Deleting user.")
         raise exception.DatastoreOperationNotSupported(
             operation='delete_user', datastore=MANAGER)
 
     def get_user(self, context, username, hostname):
+        LOG.debug("Getting user.")
         raise exception.DatastoreOperationNotSupported(
             operation='get_user', datastore=MANAGER)
 
     def grant_access(self, context, username, hostname, databases):
+        LOG.debug("Granting access.")
         raise exception.DatastoreOperationNotSupported(
             operation='grant_access', datastore=MANAGER)
 
     def revoke_access(self, context, username, hostname, database):
+        LOG.debug("Revoking access.")
         raise exception.DatastoreOperationNotSupported(
             operation='revoke_access', datastore=MANAGER)
 
     def list_access(self, context, username, hostname):
+        LOG.debug("Listing access.")
         raise exception.DatastoreOperationNotSupported(
             operation='list_access', datastore=MANAGER)
 
     def list_databases(self, context, limit=None, marker=None,
                        include_marker=False):
+        LOG.debug("Listing databases.")
         raise exception.DatastoreOperationNotSupported(
             operation='list_databases', datastore=MANAGER)
 
     def list_users(self, context, limit=None, marker=None,
                    include_marker=False):
+        LOG.debug("Listing users.")
         raise exception.DatastoreOperationNotSupported(
             operation='list_users', datastore=MANAGER)
 
     def enable_root(self, context):
+        LOG.debug("Enabling root.")
         raise exception.DatastoreOperationNotSupported(
             operation='enable_root', datastore=MANAGER)
 
     def is_root_enabled(self, context):
+        LOG.debug("Checking if root is enabled.")
         raise exception.DatastoreOperationNotSupported(
             operation='is_root_enabled', datastore=MANAGER)
 
     def get_replication_snapshot(self, context, snapshot_info,
                                  replica_source_config=None):
+        LOG.debug("Getting replication snapshot.")
         raise exception.DatastoreOperationNotSupported(
             operation='get_replication_snapshot', datastore=MANAGER)
 
     def attach_replication_slave(self, context, snapshot, slave_config):
+        LOG.debug("Attaching replica.")
         raise exception.DatastoreOperationNotSupported(
             operation='attach_replication_slave', datastore=MANAGER)
 
     def detach_replica(self, context):
+        LOG.debug("Detaching replica.")
         raise exception.DatastoreOperationNotSupported(
             operation='detach_replica', datastore=MANAGER)
 
     def demote_replication_master(self, context):
+        LOG.debug("Demoting replica source.")
         raise exception.DatastoreOperationNotSupported(
             operation='demote_replication_master', datastore=MANAGER)
