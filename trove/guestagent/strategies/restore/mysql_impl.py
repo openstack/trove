0ddd79ed3f9579f96541721991b7a4ea8d108997
Petr Malik <pmalik@tesora.com>
2015-02-18 18:16:20 -0500

    Delete mysql error log file as root on restore
    The Problem:
    The current code let's Python manage the MySQL error log file generated
    during the root password reset on backup restore (see bug/1419995).
    The problem is that the database service may assume ownership of that
    file in the process. The 'restricted deletion' policy dictates that
    only owners (and the directory owner) can delete temporary files.
    If this happens the cleanup step fails halting the whole
    restore process.
    NOTE: Not all datastores necessarily exhibit this behavior
    (i.e. taking ownership of the log file).
    It has been confirmed on Percona 5.5 running under Ubuntu Linux.
    
    The Reproduction (important):
    This issue is masked in the default testing (redstack) environment.
    Redstack (in 'scripts/functions_qemu') sets 'GUEST_LOGDIR'
    to the 'log_dir' value from 'etc/trove/trove-guestagent.conf.sample'
    which happenns to be '/tmp/'.
    Later in 'scripts/files/trove-guest.upstart.conf' assumes the ownership
    of that directory: "chown GUEST_USERNAME:root GUEST_LOGDIR"
    effectively hiding this issue (see bug/1423760).
    The temporary directory would generally not be owned by the trove user
    in a production environment.
    
    In order to reproduce this issue in Redstack set 'GUEST_LOGDIR' to
    some directory other than '/tmp', like '/var/log/trove/',
    kick-start Percona 5.5 (remember to remove the cached
    image if any) and run instance backup/restore or int-tests.
    
    The Solution:
    Do not let Trove do the cleanup on the error log file.
    Force-delete the file as root, logging but not raising any exceptions.
    
    Tested on: MySQL 5.5/5.6 and Percona 5.5/5.6
    
    Change-Id: I9dd6ed543a01ecc4f84065ea4bf3737960de6e24
    Closes-Bug: 1423759
    Related-Bug: 1423760

--- trove/guestagent/strategies/restore/mysql_impl.py
+++ trove/guestagent/strategies/restore/mysql_impl.py
@@ -128,9 +128,18 @@ class MySQLRestoreMixin(object):
             utils.execute_with_timeout("sudo", "chmod", "a+r", init_file.name)
             self._writelines_one_per_line(init_file,
                                           self.RESET_ROOT_MYSQL_COMMANDS)
-            with tempfile.NamedTemporaryFile(
-                    suffix=self._ERROR_LOG_SUFFIX) as err_log_file:
+            # Do not attempt to delete the file as the 'trove' user.
+            # The process writing into it may have assumed its ownership.
+            # Only owners can delete temporary
+            # files (restricted deletion).
+            err_log_file = tempfile.NamedTemporaryFile(
+                suffix=self._ERROR_LOG_SUFFIX,
+                delete=False)
+            try:
                 self._start_mysqld_safe_with_init_file(init_file, err_log_file)
+            finally:
+                err_log_file.close()
+                MySQLRestoreMixin._delete_file(err_log_file.name)
 
     def _writelines_one_per_line(self, fp, lines):
         fp.write(os.linesep.join(lines))
@@ -143,6 +152,20 @@ class MySQLRestoreMixin(object):
                     self._ERROR_MESSAGE_PATTERN
                 )
         return None
+
+    @classmethod
+    def _delete_file(self, file_path):
+        """Force-remove a given file as root.
+        Do not raise an exception on failure.
+        """
+
+        if os.path.isfile(file_path):
+            try:
+                utils.execute_with_timeout("rm", "-f", file_path,
+                                           run_as_root=True,
+                                           root_helper="sudo")
+            except Exception:
+                LOG.exception("Could not remove file: '%s'" % file_path)
 
     @classmethod
     def _is_non_zero_file(self, fp):
