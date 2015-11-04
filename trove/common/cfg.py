68c1c61b5492ede70cc4187c48fb7b7525185653
Craig Vyvial <cp16net@gmail.com>
2015-11-04 17:15:07 -0600

    fix the version info of trove to use pbr
    Change-Id: I2a445cdea20e70399e331ea4e97042ecd0137c2e
    Closes-Bug: #1513259

--- trove/common/cfg.py
+++ trove/common/cfg.py
@@ -20,7 +20,7 @@ import os.path
 from oslo_config import cfg
 from oslo_log import log as logging
 
-import trove
+from trove.version import version_info as version
 
 
 UNKNOWN_SERVICE_ID = 'unknown-service-id-error'
@@ -1200,5 +1200,5 @@ def custom_parser(parsername, parser):
 def parse_args(argv, default_config_files=None):
     cfg.CONF(args=argv[1:],
              project='trove',
-             version=trove.__version__,
+             version=version.cached_version_string(),
              default_config_files=default_config_files)
