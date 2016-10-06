18f23a71777cdab1a397cafe0a677d9692eccacd
Morgan Jones <morgan@parelastic.com>
2016-10-06 16:14:42 -0400

    Fix RPC Versioning
    Modifies how the APIs for Conductor, Guest, and Taskmanager
    handle versioning for olso_messaging.
    
    Closes-Bug: 1631106
    Change-Id: I2b9d50e15ac98041c427b74cac63ac231cb4b53e
    Co-Authored-By: Amrith Kumar <amrith@tesora.com>

--- trove/common/strategies/cluster/experimental/cassandra/guestagent.py
+++ trove/common/strategies/cluster/experimental/cassandra/guestagent.py
@@ -30,65 +30,100 @@ class CassandraGuestAgentStrategy(base.BaseGuestAgentStrategy):
 
 
 class CassandraGuestAgentAPI(guest_api.API):
+    """Cluster Specific Datastore Guest API
+
+    **** VERSION CONTROLLED API ****
+
+    The methods in this class are subject to version control as
+    coordinated by guestagent/api.py.  Whenever a change is made to
+    any API method in this class, add a version number and comment
+    to the top of guestagent/api.py and use the version number as
+    appropriate in this file
+    """
 
     def get_data_center(self):
         LOG.debug("Retrieving the data center for node: %s" % self.id)
+        version = guest_api.API.API_BASE_VERSION
+
         return self._call("get_data_center", guest_api.AGENT_LOW_TIMEOUT,
-                          self.version_cap)
+                          version=version)
 
     def get_rack(self):
         LOG.debug("Retrieving the rack for node: %s" % self.id)
+        version = guest_api.API.API_BASE_VERSION
+
         return self._call("get_rack", guest_api.AGENT_LOW_TIMEOUT,
-                          self.version_cap)
+                          version=version)
 
     def set_seeds(self, seeds):
         LOG.debug("Configuring the gossip seeds for node: %s" % self.id)
+        version = guest_api.API.API_BASE_VERSION
+
         return self._call("set_seeds", guest_api.AGENT_LOW_TIMEOUT,
-                          self.version_cap, seeds=seeds)
+                          version=version, seeds=seeds)
 
     def get_seeds(self):
         LOG.debug("Retrieving the gossip seeds for node: %s" % self.id)
+        version = guest_api.API.API_BASE_VERSION
+
         return self._call("get_seeds", guest_api.AGENT_LOW_TIMEOUT,
-                          self.version_cap)
+                          version=version)
 
     def set_auto_bootstrap(self, enabled):
         LOG.debug("Setting the auto-bootstrap to '%s' for node: %s"
                   % (enabled, self.id))
+        version = guest_api.API.API_BASE_VERSION
+
         return self._call("set_auto_bootstrap", guest_api.AGENT_LOW_TIMEOUT,
-                          self.version_cap, enabled=enabled)
+                          version=version, enabled=enabled)
 
     def cluster_complete(self):
         LOG.debug("Sending a setup completion notification for node: %s"
                   % self.id)
+        version = guest_api.API.API_BASE_VERSION
+
         return self._call("cluster_complete", guest_api.AGENT_HIGH_TIMEOUT,
-                          self.version_cap)
+                          version=version)
 
     def node_cleanup_begin(self):
         LOG.debug("Signaling the node to prepare for cleanup: %s" % self.id)
+        version = guest_api.API.API_BASE_VERSION
+
         return self._call("node_cleanup_begin", guest_api.AGENT_LOW_TIMEOUT,
-                          self.version_cap)
+                          version=version)
 
     def node_cleanup(self):
         LOG.debug("Running cleanup on node: %s" % self.id)
-        return self._cast('node_cleanup', self.version_cap)
+        version = guest_api.API.API_BASE_VERSION
+
+        return self._cast('node_cleanup', version=version)
 
     def node_decommission(self):
         LOG.debug("Decommission node: %s" % self.id)
-        return self._cast("node_decommission", self.version_cap)
+        version = guest_api.API.API_BASE_VERSION
+
+        return self._cast("node_decommission", version=version)
 
     def cluster_secure(self, password):
         LOG.debug("Securing the cluster via node: %s" % self.id)
+        version = guest_api.API.API_BASE_VERSION
+
         return self._call(
             "cluster_secure", guest_api.AGENT_HIGH_TIMEOUT,
-            self.version_cap, password=password)
+            version=version, password=password)
 
     def get_admin_credentials(self):
         LOG.debug("Retrieving the admin credentials from node: %s" % self.id)
+        version = guest_api.API.API_BASE_VERSION
+
         return self._call("get_admin_credentials", guest_api.AGENT_LOW_TIMEOUT,
-                          self.version_cap)
+                          version=version)
 
     def store_admin_credentials(self, admin_credentials):
         LOG.debug("Storing the admin credentials on node: %s" % self.id)
+        version = guest_api.API.API_BASE_VERSION
+
         return self._call("store_admin_credentials",
-                          guest_api.AGENT_LOW_TIMEOUT, self.version_cap,
+                          guest_api.AGENT_LOW_TIMEOUT,
+                          version=version,
                           admin_credentials=admin_credentials)
--- trove/conductor/api.py
+++ trove/conductor/api.py
@@ -16,28 +16,50 @@ from oslo_log import log as logging
 import oslo_messaging as messaging
 
 from trove.common import cfg
-from trove.common.rpc import version as rpc_version
 from trove.common.serializable_notification import SerializableNotification
 from trove import rpc
-
 
 CONF = cfg.CONF
 LOG = logging.getLogger(__name__)
 
 
 class API(object):
-    """API for interacting with trove conductor."""
+    """API for interacting with trove conductor.
+
+    API version history:
+        * 1.0 - Initial version.
+
+    When updating this API, also update API_LATEST_VERSION
+    """
+
+    # API_LATEST_VERSION should bump the minor number each time
+    # a method signature is added or changed
+    API_LATEST_VERSION = '1.0'
+
+    # API_BASE_VERSION should only change on major version upgrade
+    API_BASE_VERSION = '1.0'
+
+    VERSION_ALIASES = {
+        'icehouse': '1.0',
+        'juno': '1.0',
+        'kilo': '1.0',
+        'liberty': '1.0',
+        'mitaka': '1.0',
+        'newton': '1.0',
+
+        'latest': API_LATEST_VERSION
+    }
 
     def __init__(self, context):
         self.context = context
         super(API, self).__init__()
 
+        version_cap = self.VERSION_ALIASES.get(
+            CONF.upgrade_levels.conductor, CONF.upgrade_levels.conductor)
         target = messaging.Target(topic=CONF.conductor_queue,
-                                  version=rpc_version.RPC_API_VERSION)
+                                  version=version_cap)
 
-        self.version_cap = rpc_version.VERSION_ALIASES.get(
-            CONF.upgrade_levels.conductor)
-        self.client = self.get_client(target, self.version_cap)
+        self.client = self.get_client(target, version_cap)
 
     def get_client(self, target, version_cap, serializer=None):
         return rpc.get_client(target,
@@ -47,8 +69,9 @@ class API(object):
     def heartbeat(self, instance_id, payload, sent=None):
         LOG.debug("Making async call to cast heartbeat for instance: %s"
                   % instance_id)
+        version = self.API_BASE_VERSION
 
-        cctxt = self.client.prepare(version=self.version_cap)
+        cctxt = self.client.prepare(version=version)
         cctxt.cast(self.context, "heartbeat",
                    instance_id=instance_id,
                    sent=sent,
@@ -58,8 +81,9 @@ class API(object):
                       **backup_fields):
         LOG.debug("Making async call to cast update_backup for instance: %s"
                   % instance_id)
+        version = self.API_BASE_VERSION
 
-        cctxt = self.client.prepare(version=self.version_cap)
+        cctxt = self.client.prepare(version=version)
         cctxt.cast(self.context, "update_backup",
                    instance_id=instance_id,
                    backup_id=backup_id,
@@ -69,14 +93,16 @@ class API(object):
     def report_root(self, instance_id, user):
         LOG.debug("Making async call to cast report_root for instance: %s"
                   % instance_id)
-        cctxt = self.client.prepare(version=self.version_cap)
+        version = self.API_BASE_VERSION
+        cctxt = self.client.prepare(version=version)
         cctxt.cast(self.context, "report_root",
                    instance_id=instance_id,
                    user=user)
 
     def notify_end(self, **notification_args):
         LOG.debug("Making async call to cast end notification")
-        cctxt = self.client.prepare(version=self.version_cap)
+        version = self.API_BASE_VERSION
+        cctxt = self.client.prepare(version=version)
         context = self.context
         serialized = SerializableNotification.serialize(context,
                                                         context.notification)
@@ -86,7 +112,8 @@ class API(object):
 
     def notify_exc_info(self, message, exception):
         LOG.debug("Making async call to cast error notification")
-        cctxt = self.client.prepare(version=self.version_cap)
+        version = self.API_BASE_VERSION
+        cctxt = self.client.prepare(version=version)
         context = self.context
         serialized = SerializableNotification.serialize(context,
                                                         context.notification)
