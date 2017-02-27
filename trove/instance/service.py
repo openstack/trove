Tesora/tesora-trove

Fix module-instances command
Fixed the module-instances command to return a paginated
list of instances.  Also added a --count_only flag to the
command to return a summary of the applied instances
based on the MD5 of the module (this is most useful
for live_update modules, to see which ones haven't been
updated).

Also cleaned up the code a bit, putting some methods
into files where they made more sense (and would cause
less potential collisions during import).

Change-Id: I512bf69c10c01c576476f5e80a4cb367b42c0103
Depends-On: I9af513ed032b52768bab8d4d85bdfd0e2ffd77bg
Closes-Bug: #1554900
Partial-Bug: DBAAS-2103

trove/instance/service.py
@@ -609,8 +609,9 @@ class InstanceController(wsgi.Controller):
         self.authorize_instance_action(context, 'module_apply', instance)
         module_ids = [mod['id'] for mod in body.get('modules', [])]
         modules = module_models.Modules.load_by_ids(context, module_ids)
-        module_list = models.validate_modules(
+        module_models.Modules.validate(
             modules, instance.datastore.id, instance.datastore_version.id)
+        module_list = module_views.convert_modules(modules)
         client = create_guest_client(context, id)
         result_list = client.module_apply(module_list)
         models.Instance.add_instance_modules(context, id, modules)

trove/instance/views.py
@@ -43,6 +43,8 @@ class InstanceView(object):
                           "version": self.instance.datastore_version.name},
             "region": self.instance.region_name
         }
+        if self.context.is_admin:
+            instance_dict['tenant_id'] = self.instance.tenant_id
         if self.instance.volume_support:
             instance_dict['volume'] = {'size': self.instance.volume_size}
 
@@ -218,3 +220,19 @@ class GuestLogsView(object):
 
     def data(self):
         return [GuestLogView(l).data() for l in self.guest_logs]
+
+
+def convert_instance_count(instance_count):
+    instance_list = []
+    for row in instance_count:
+        (_, name, id, md5, count, current, min_date, max_date) = row
+        instance_list.append(
+            {'module_name': name,
+             'module_id': id,
+             'module_md5': md5,
+             'instance_count': count,
+             'current': current,
+             'min_updated_date': min_date,
+             'max_updated_date': max_date
+             })
+    return instance_list

trove/module/service.py
@@ -64,7 +64,7 @@ class ModuleController(wsgi.Controller):
         return wsgi.Result(view.data(), 200)
 
     def show(self, req, tenant_id, id):
-        LOG.info(_("Showing module %s") % id)
+        LOG.info(_("Showing module %s.") % id)
 
         context = req.environ[wsgi.CONTEXT_KEY]
         module = models.Module.load(context, id)
@@ -106,7 +106,7 @@ class ModuleController(wsgi.Controller):
         return wsgi.Result(view_data.data(), 200)
 
     def delete(self, req, tenant_id, id):
-        LOG.info(_("Deleting module %s") % id)
+        LOG.info(_("Deleting module %s.") % id)
 
         context = req.environ[wsgi.CONTEXT_KEY]
         module = models.Module.load(context, id)
@@ -115,7 +115,7 @@ class ModuleController(wsgi.Controller):
         return wsgi.Result(None, 200)
 
     def update(self, req, body, tenant_id, id):
-        LOG.info(_("Updating module %s") % id)
+        LOG.info(_("Updating module %s.") % id)
 
         context = req.environ[wsgi.CONTEXT_KEY]
         module = models.Module.load(context, id)
@@ -173,26 +173,33 @@ class ModuleController(wsgi.Controller):
         return wsgi.Result(view_data.data(), 200)
 
     def instances(self, req, tenant_id, id):
-        LOG.info(_("Getting instances for module %s") % id)
+        LOG.info(_("Getting instances for module %s.") % id)
 
         context = req.environ[wsgi.CONTEXT_KEY]
 
         module = models.Module.load(context, id)
-        self.authorize_module_action(context, 'update', module)
-
-        instance_modules, marker = models.InstanceModules.load(
-            context, module_id=id)
-        if instance_modules:
-            instance_ids = [inst_mod.instance_id
-                            for inst_mod in instance_modules]
-            include_clustered = (
-                req.GET.get('include_clustered', '').lower() == 'true')
-            instances, marker = instance_models.Instances.load(
-                context, include_clustered, instance_ids=instance_ids)
+        self.authorize_module_action(context, 'instances', module)
+        count_only = req.GET.get('count_only', '').lower() == 'true'
+        include_clustered = (
+            req.GET.get('include_clustered', '').lower() == 'true')
+        if count_only:
+            instance_count = instance_models.module_instance_count(
+                context, id, include_clustered=include_clustered)
+            result_list = {
+                'instances':
+                instance_views.convert_instance_count(instance_count)}
         else:
-            instances = []
-            marker = None
-        view = instance_views.InstancesView(instances, req=req)
-        paged = pagination.SimplePaginatedDataView(req.url, 'instances',
-                                                   view, marker)
-        return wsgi.Result(paged.data(), 200)
+            instance_modules, marker = models.InstanceModules.load(
+                context, module_id=id)
+            if instance_modules:
+                instance_ids = [inst_mod.instance_id
+                                for inst_mod in instance_modules]
+                instances, marker = instance_models.Instances.load(
+                    context, include_clustered, instance_ids=instance_ids)
+            else:
+                instances = []
+                marker = None
+            view = instance_views.InstancesView(instances, req=req)
+            result_list = pagination.SimplePaginatedDataView(
+                req.url, 'instances', view, marker).data()
+        return wsgi.Result(result_list, 200)

openstack/trove

Change-Id: I963e0f03875a1b93e2e1214bcb6580c507fa45fe

trove/instance/service.py
@@ -536,9 +536,9 @@ class InstanceController(wsgi.Controller):
         self.authorize_instance_action(context, 'module_apply', instance)
         module_ids = [mod['id'] for mod in body.get('modules', [])]
         modules = module_models.Modules.load_by_ids(context, module_ids)
-        models.validate_modules_for_apply(
+        module_models.Modules.validate(
             modules, instance.datastore.id, instance.datastore_version.id)
-        module_list = module_views.get_module_list(modules)
+        module_list = module_views.convert_modules_to_list(modules)
         client = create_guest_client(context, id)
         result_list = client.module_apply(module_list)
         models.Instance.add_instance_modules(context, id, modules)

trove/instance/views.py
@@ -41,6 +41,8 @@ class InstanceView(object):
                           "version": self.instance.datastore_version.name},
             "region": self.instance.region_name
         }
+        if self.context.is_admin:
+            instance_dict['tenant_id'] = self.instance.tenant_id
         if self.instance.volume_support:
             instance_dict['volume'] = {'size': self.instance.volume_size}
 
@@ -212,3 +214,19 @@ class GuestLogsView(object):
 
     def data(self):
         return [GuestLogView(l).data() for l in self.guest_logs]
+
+
+def convert_instance_count_to_list(instance_count):
+    instance_list = []
+    for row in instance_count:
+        (_, name, id, md5, count, current, min_date, max_date) = row
+        instance_list.append(
+            {'module_name': name,
+             'module_id': id,
+             'module_md5': md5,
+             'instance_count': count,
+             'current': current,
+             'min_updated_date': min_date,
+             'max_updated_date': max_date
+             })
+    return instance_list

trove/module/service.py
@@ -62,7 +62,7 @@ class ModuleController(wsgi.Controller):
         return wsgi.Result(view.data(), 200)
 
     def show(self, req, tenant_id, id):
-        LOG.info(_("Showing module %s") % id)
+        LOG.info(_("Showing module %s.") % id)
 
         context = req.environ[wsgi.CONTEXT_KEY]
         module = models.Module.load(context, id)
@@ -104,7 +104,7 @@ class ModuleController(wsgi.Controller):
         return wsgi.Result(view_data.data(), 200)
 
     def delete(self, req, tenant_id, id):
-        LOG.info(_("Deleting module %s") % id)
+        LOG.info(_("Deleting module %s.") % id)
 
         context = req.environ[wsgi.CONTEXT_KEY]
         module = models.Module.load(context, id)
@@ -113,7 +113,7 @@ class ModuleController(wsgi.Controller):
         return wsgi.Result(None, 200)
 
     def update(self, req, body, tenant_id, id):
-        LOG.info(_("Updating module %s") % id)
+        LOG.info(_("Updating module %s.") % id)
 
         context = req.environ[wsgi.CONTEXT_KEY]
         module = models.Module.load(context, id)
@@ -171,26 +171,34 @@ class ModuleController(wsgi.Controller):
         return wsgi.Result(view_data.data(), 200)
 
     def instances(self, req, tenant_id, id):
-        LOG.info(_("Getting instances for module %s") % id)
+        LOG.info(_("Getting instances for module %s.") % id)
 
         context = req.environ[wsgi.CONTEXT_KEY]
 
         module = models.Module.load(context, id)
         self.authorize_module_action(context, 'instances', module)
 
-        instance_modules, marker = models.InstanceModules.load(
-            context, module_id=id)
-        if instance_modules:
-            instance_ids = [inst_mod.instance_id
-                            for inst_mod in instance_modules]
-            include_clustered = (
-                req.GET.get('include_clustered', '').lower() == 'true')
-            instances, marker = instance_models.Instances.load(
-                context, include_clustered, instance_ids=instance_ids)
+        count_only = req.GET.get('count_only', '').lower() == 'true'
+        include_clustered = (
+            req.GET.get('include_clustered', '').lower() == 'true')
+        if count_only:
+            instance_count = instance_models.module_instance_count(
+                context, id, include_clustered=include_clustered)
+            result_list = {
+                'instances':
+                instance_views.convert_instance_count_to_list(instance_count)}
         else:
-            instances = []
-            marker = None
-        view = instance_views.InstancesView(instances, req=req)
-        paged = pagination.SimplePaginatedDataView(req.url, 'instances',
-                                                   view, marker)
-        return wsgi.Result(paged.data(), 200)
+            instance_modules, marker = models.InstanceModules.load(
+                context, module_id=id)
+            if instance_modules:
+                instance_ids = [inst_mod.instance_id
+                                for inst_mod in instance_modules]
+                instances, marker = instance_models.Instances.load(
+                    context, include_clustered, instance_ids=instance_ids)
+            else:
+                instances = []
+                marker = None
+            view = instance_views.InstancesView(instances, req=req)
+            result_list = pagination.SimplePaginatedDataView(
+                req.url, 'instances', view, marker).data()
+        return wsgi.Result(result_list, 200)