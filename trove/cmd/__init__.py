56bb6f39b6c808160d2e4c96fcc2104ba1daf817
Corey Bryant <corey.bryant@canonical.com>
2020-05-05 15:42:31 -0400

    Monkey patch original current_thread _active
    Monkey patch the original current_thread to use the up-to-date _active
    global variable. This solution is based on that documented at:
    https://github.com/eventlet/eventlet/issues/592
    
    Change-Id: I61e5e270bf66b0355da3282c19cbc9fd42c4090b
    Closes-Bug: #1863021

--- trove/cmd/__init__.py
+++ trove/cmd/__init__.py
@@ -26,3 +26,9 @@ import os
 if not os.environ.get('NO_EVENTLET_MONKEYPATCH'):
     import eventlet
     eventlet.monkey_patch(all=True)
+    # Monkey patch the original current_thread to use the up-to-date _active
+    # global variable. See https://bugs.launchpad.net/bugs/1863021 and
+    # https://github.com/eventlet/eventlet/issues/592
+    import __original_module_threading as orig_threading # noqa
+    import threading  # noqa
+    orig_threading.current_thread.__globals__['_active'] = threading._active
