40191dd5be192a9a0b70f884e0a110cb0a18b669
wu.chunyang <wuchunyang@yovole.com>
2022-09-02 22:16:56 +0800

    Change settings for stable/xena
    ref: https://github.com/openstack/trove/commit/d98cd1532faf07d863320fe0f101c6c18cd95471
    Change-Id: I615574dade0526aaeb6707f0bbcde6a55ed2a345

--- trove/common/cfg.py
+++ trove/common/cfg.py
@@ -1444,7 +1444,7 @@ rpcapi_cap_opts = [
         'taskmanager', default='latest',
         help='Set a version cap for messages sent to taskmanager services'),
     cfg.StrOpt(
-        'guestagent', default='latest',
+        'guestagent', default='xena',
         help='Set a version cap for messages sent to guestagent services'),
     cfg.StrOpt(
         'conductor', default='latest',
