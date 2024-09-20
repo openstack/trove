62857880c3a5a0863eb4b7192b9f9abf1eae7594
Adrian Jarvis <adrian.jarvis@catalystcloud.nz>
2024-09-20 12:21:43 +1200

    Add support for PostgreSQL 13 and above.
    Parses the version from the DataStoreVersion name and exposes to the
    configuration templates to allow conditional tests based on components
    of the datastore version. For PostgresSQL version 12 and below sets the
    wal_keep_size and for version 13 and above sets wal_keep_segments
    instead.
    Closes-Bug: #2008285
    
    Change-Id: Ic028e4e0b825f4e5fc404b0aa05f9db61367e347
    (cherry picked from commit 9ae2392d5a3b660529d35e1d8e84744d00088727)

--- trove/tests/unittests/common/test_template.py
+++ trove/tests/unittests/common/test_template.py
@@ -12,10 +12,24 @@
 import re
 
 from unittest.mock import Mock
+from unittest.mock import patch
+
+from semantic_version import Version
 
 from trove.common import template
+from trove.common.utils import ENV
 from trove.datastore.models import DatastoreVersion
 from trove.tests.unittests import trove_testtools
+
+
+def mock_datastore_version(datastore_name='MySQL', name='mysql-5.7',
+                           manager='mysql', version=''):
+    datastore = Mock(spec=DatastoreVersion)
+    datastore.datastore_name = datastore_name
+    datastore.name = name
+    datastore.manager = manager
+    datastore.version = version
+    return datastore
 
 
 class TemplateTest(trove_testtools.TestCase):
@@ -58,10 +72,7 @@ class TemplateTest(trove_testtools.TestCase):
                                self.server_id)
 
     def test_single_instance_config_rendering(self):
-        datastore = Mock(spec=DatastoreVersion)
-        datastore.datastore_name = 'MySql'
-        datastore.name = 'mysql-5.7'
-        datastore.manager = 'mysql'
+        datastore = mock_datastore_version()
         config = template.SingleInstanceConfigTemplate(datastore,
                                                        self.flavor_dict,
                                                        self.server_id)
@@ -70,10 +81,9 @@ class TemplateTest(trove_testtools.TestCase):
 
     def test_renderer_discovers_special_config(self):
         """Finds our special config file for the version 'mysql-test'."""
-        datastore = Mock(spec=DatastoreVersion)
-        datastore.datastore_name = 'mysql'
-        datastore.name = 'mysql-test'
-        datastore.manager = 'mysql'
+        datastore = mock_datastore_version(name='mysql-test',
+                                           manager='mysql',
+                                           datastore_name='mysql')
         config = template.SingleInstanceConfigTemplate(datastore,
                                                        self.flavor_dict,
                                                        self.server_id)
@@ -81,21 +91,106 @@ class TemplateTest(trove_testtools.TestCase):
                                {'ram': 0}, self.server_id)
 
     def test_replica_source_config_rendering(self):
-        datastore = Mock(spec=DatastoreVersion)
-        datastore.datastore_name = 'MySql'
-        datastore.name = 'mysql-5.7'
-        datastore.manager = 'mysql'
+        datastore = mock_datastore_version()
         config = template.ReplicaSourceConfigTemplate(datastore,
                                                       self.flavor_dict,
                                                       self.server_id)
         self.assertTrue(self._find_in_template(config.render(), "log_bin"))
 
     def test_replica_config_rendering(self):
-        datastore = Mock(spec=DatastoreVersion)
-        datastore.datastore_name = 'MySql'
-        datastore.name = 'mysql-5.7'
-        datastore.manager = 'mysql'
+        datastore = mock_datastore_version()
         config = template.ReplicaConfigTemplate(datastore,
                                                 self.flavor_dict,
                                                 self.server_id)
         self.assertTrue(self._find_in_template(config.render(), "relay_log"))
+
+    def test_replica_config_rendering_mysql_v8(self):
+        datastore = mock_datastore_version(name='8.0')
+        config = template.ReplicaConfigTemplate(datastore,
+                                                self.flavor_dict,
+                                                self.server_id)
+        self.assertTrue(self._find_in_template(config.render(),
+                                               "binlog_format"))
+
+    def test_config_postgresql_pre_v13_sets_wal_keep_segments(self):
+        datastore = mock_datastore_version(datastore_name='PostgreSQL',
+                                           name='12.17',
+                                           manager='postgresql')
+        config = template.SingleInstanceConfigTemplate(datastore,
+                                                       self.flavor_dict,
+                                                       self.server_id)
+        self.assertTrue(
+            self._find_in_template(config.render(), "wal_keep_segments"))
+        self.assertIsNone(
+            self._find_in_template(config.render(), "wal_keep_size"))
+
+    def test_config_postgresql_post_v12_sets_wal_keep_size(self):
+        datastore = mock_datastore_version(datastore_name='PostgreSQL',
+                                           name='13.2',
+                                           manager='postgresql')
+        config = template.SingleInstanceConfigTemplate(datastore,
+                                                       self.flavor_dict,
+                                                       self.server_id)
+        self.assertTrue(
+            self._find_in_template(config.render(), "wal_keep_size"))
+        self.assertIsNone(
+            self._find_in_template(config.render(), "wal_keep_segments"))
+
+    def test_parse_version_name_missing_release_number(self):
+        datastore = mock_datastore_version(datastore_name='PostgreSQL',
+                                           name='test',
+                                           manager='postgresql')
+        config = template.SingleInstanceConfigTemplate(datastore,
+                                                       self.flavor_dict,
+                                                       self.server_id)
+        self.assertEqual(Version(major=0, minor=0, patch=0),
+                         config._parse_datastore_version())
+
+    def test_parse_version_release_part_of_name(self):
+        datastore = mock_datastore_version(datastore_name='PostgreSQL',
+                                           name='test-12.17',
+                                           manager='postgresql')
+        config = template.SingleInstanceConfigTemplate(datastore,
+                                                       self.flavor_dict,
+                                                       self.server_id)
+        self.assertEqual(Version(major=0, minor=0, patch=0),
+                         config._parse_datastore_version())
+
+    def test_parse_version_name_with_prefix(self):
+        datastore = mock_datastore_version(datastore_name='PostgreSQL',
+                                           name='16.1-test',
+                                           manager='postgresql')
+        config = template.SingleInstanceConfigTemplate(datastore,
+                                                       self.flavor_dict,
+                                                       self.server_id)
+        self.assertEqual(Version(major=16, minor=1, patch=0,
+                                 prerelease=('test',)),
+                         config._parse_datastore_version())
+
+    def test_parse_version_prefer_version_if_set(self):
+        datastore = mock_datastore_version(datastore_name='PostgreSQL',
+                                           name='test',
+                                           manager='postgresql',
+                                           version='16.1')
+        config = template.SingleInstanceConfigTemplate(datastore,
+                                                       self.flavor_dict,
+                                                       self.server_id)
+        self.assertEqual(Version(major=16, minor=1, patch=0,
+                                 ),
+                         config._parse_datastore_version())
+
+    def test_template_paths(self):
+        datastore = mock_datastore_version(name='5.7.9')
+        config = template.SingleInstanceConfigTemplate(datastore,
+                                                       self.flavor_dict,
+                                                       self.server_id)
+        with patch.object(ENV, 'select_template') as select_template_mock:
+            config.get_template()
+            select_template_mock.assert_called_with(
+                ['MySQL/5.7.9/config.template',
+                 'MySQL/5.7/config.template',
+                 'MySQL/5/config.template',
+                 'MySQL/config.template',
+                 'mysql/config.template'
+                 ]
+            )
