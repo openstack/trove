015a8a7cb49df65640f5b382660c9af4c094b173
wu.chunyang <wchy1001@gmail.com>
2025-12-23 20:18:16 +0800

    Set ipc_mode to host to prevent potential shared memory exhaustion
    Trove is launching Docker containers without specifying shm_size, which
    left the default 64MB limit for /dev/shm. PostgreSQL uses shared_buffers
    of 128MB by default, so any database running with default settings could
    exceed the default /dev/shm size. In this situation PostgreSQL may fail
    during shared memory operations with errors such as: "could not resize
    shared memory segment: No space left on device".
    
    This patch uses ipc_mode=host to pass the shared memory from host to container.
    
    Co-Authored-By: Eric <mangust404@gmail.com>
    
    Change-Id: I2a7c69776e2a41f9ac5fd9da93f5772746d87a2f
    Signed-off-by: wu.chunyang <wchy1001@gmail.com>
    (cherry picked from commit 124d89a896d80e6ae9d6cc9b3792de9cc8f730ee)

--- trove/guestagent/utils/docker.py
+++ trove/guestagent/utils/docker.py
@@ -82,6 +82,8 @@ def _create_container_with_low_level_api(image: str, param: dict) -> None:
         host_config_kwargs["privileged"] = param.get("privileged")
     if param.get("volumes"):
         host_config_kwargs["binds"] = param.get("volumes")
+    if param.get("ipc_mode"):
+        host_config_kwargs["ipc_mode"] = param.get("ipc_mode")
     host_config = client.create_host_config(**host_config_kwargs)
 
     network_config_kwargs = dict()
@@ -162,6 +164,7 @@ def start_container(client, image, name="database",
                   volumes=volumes,
                   ports=ports,
                   user=user,
+                  ipc_mode="host",
                   environment=environment,
                   healthcheck=healthcheck,
                   command=command)
