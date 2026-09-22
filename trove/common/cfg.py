1c38e0992447d20e6a35b6f0eabc4631302a144d
Ivan Anfimov <lazekteam@gmail.com>
2026-09-22 21:33:58 +0000

    Update settings for 2026.2 Hibiscus
    Change-Id: Iaaa5253dc1a09f1b8b0ad6da8796175ed6c2bf0d
    Signed-off-by: Ivan Anfimov <lazekteam@gmail.com>

--- trove/common/cfg.py
+++ trove/common/cfg.py
@@ -1480,7 +1480,7 @@ rpcapi_cap_opts = [
         'taskmanager', default='latest',
         help='Set a version cap for messages sent to taskmanager services'),
     cfg.StrOpt(
-        'guestagent', default='latest',
+        'guestagent', default='hibiscus',
         help='Set a version cap for messages sent to guestagent services'),
     cfg.StrOpt(
         'conductor', default='latest',
